0683ce5a5392d31fe552e432698893152c67394a
Ilham-Nur <ilonoer26@gmail.com>
2025-03-25 21:53:14 +0700

    push to test

--- routes/web.php
+++ routes/web.php
@@ -14,42 +14,46 @@ Route::get('/', [LoginController::class, 'showLoginForm'])->name('showLoginForm'
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/dashboardnew', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
 
-// Jenis Kendaraan
-Route::get('/jeniskendaraan', [JenisKendaraanController::class, 'index'])->name('jenis-kendaraan');
-Route::post('/jeniskendaraan/store', [JenisKendaraanController::class, 'store']);
-Route::put('/jeniskendaraan/update/{id}', [JenisKendaraanController::class, 'update']);
-Route::delete('/jeniskendaraan/destroy/{id}', [JenisKendaraanController::class, 'destroy']);
+    // Dashboard
+    Route::get('/dashboardnew', [DashboardController::class, 'index'])->name('dashboard');
 
-// Kategori
-Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori');
-route::post('/kategori/store', [KategoriController::class, 'store']);
-route::put('/kategori/update/{id}', [KategoriController::class, 'update']);
-route::delete('/kategori/destroy/{id}', [KategoriController::class, 'destroy']);
+    // Jenis Kendaraan
+    Route::get('/jeniskendaraan', [JenisKendaraanController::class, 'index'])->name('jenis-kendaraan');
+    Route::post('/jeniskendaraan/store', [JenisKendaraanController::class, 'store']);
+    Route::put('/jeniskendaraan/update/{id}', [JenisKendaraanController::class, 'update']);
+    Route::delete('/jeniskendaraan/destroy/{id}', [JenisKendaraanController::class, 'destroy']);
+
+    // Kategori
+    Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori');
+    route::post('/kategori/store', [KategoriController::class, 'store']);
+    route::put('/kategori/update/{id}', [KategoriController::class, 'update']);
+    route::delete('/kategori/destroy/{id}', [KategoriController::class, 'destroy']);
 
 
+    // Lokasi
+    Route::get('/lokasi', [LokasiController::class, 'index'])->name('lokasi');
+    Route::post('/lokasi/store', [LokasiController::class, 'store']);
+    Route::put('/lokasi/update/{id}', [LokasiController::class, 'update']);
+    Route::delete('/lokasi/destroy/{id}', [LokasiController::class, 'destroy']);
 
-// Lokasi
-Route::get('/lokasi', [LokasiController::class, 'index'])->name('lokasi');
-Route::post('/lokasi/store', [LokasiController::class, 'store']);
-Route::put('/lokasi/update/{id}', [LokasiController::class, 'update']);
-Route::delete('/lokasi/destroy/{id}', [LokasiController::class, 'destroy']);
+    // Sp
+    Route::get('/sp', [SpController::class, 'index'])->name('sp');
+    Route::post('/sp/store', [SpController::class, 'store']);
+    Route::put('/sp/update/{id}', [SpController::class, 'update']);
+    Route::delete('/sp/destroy/{id}', [SpController::class, 'destroy']);
 
-// Sp
-Route::get('/sp', [SpController::class, 'index'])->name('sp');
-Route::post('/sp/store', [SpController::class, 'store']);
-Route::put('/sp/update/{id}', [SpController::class, 'update']);
-Route::delete('/sp/destroy/{id}', [SpController::class, 'destroy']);
+    // Supplier
+    Route::get('/supplier', [SupplierController::class, 'index'])->name('supplier');
+    Route::post('/supplier/store', [SupplierController::class, 'store']);
+    Route::put('/supplier/update/{id}', [SupplierController::class, 'update']);
+    Route::delete('/supplier/destroy/{id}', [SupplierController::class, 'destroy']);
 
-// Supplier
-Route::get('/supplier', [SupplierController::class, 'index'])->name('supplier');
-Route::post('/supplier/store', [SupplierController::class, 'store']);
-Route::put('/supplier/update/{id}', [SupplierController::class, 'update']);
-Route::delete('/supplier/destroy/{id}', [SupplierController::class, 'destroy']);
+    // User
+    Route::get('/user', [UserController::class, 'index'])->name('user');
+});
 
-// User
-Route::get('/user', [UserController::class, 'index'])->name('user');
 
 
 
